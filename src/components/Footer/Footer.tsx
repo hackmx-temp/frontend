c865ad157b41e82a0a886248437adfb6b982984d
emisaar <emisaar0406@gmail.com>
2023-09-29 22:31:21 -0600

    Footer is not ready

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,198 @@
-import React from 'react'
+import {
+  FooterBelowContainer,
+  FooterContainer,
+  FooterCopyrightContainer,
+  FooterCopyrightIcon,
+  FooterCopyrightText,
+  FooterDivider,
+  FooterLink,
+  FooterLinkContainer,
+  FooterLinkWrapper,
+  FooterLogoImg,
+  FooterLogoImgContainer,
+  FooterUpperContainer,
+  FooterWrapper,
+  LogoContainer,
+  TextContainer,
+  UPText,
+} from "./styles";
 
-const Footer = () => {
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import HubReserveLogo from "../../assets/hackmx-logo.png";
+import React, { useEffect } from "react";
+import { Grid, IconButton } from "@mui/material";
+import { Facebook, LinkedIn } from "@mui/icons-material";
+import { useLocation, useNavigate } from "react-router-dom";
+// import { ROUTES } from "../../routes/constants";
+import { FooterProps } from "./types";
+
+/**
+ * Footer for all pages in the project. Does not receive any arguments.
+ */
+
+const Footer: React.FC<FooterProps> = ({
+  variant = "",
+}) => {
+  // const navigate = useNavigate();
+  const isMobile = window.innerWidth <= 768;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    // Siempre que cambie la ubicación, desplázate al inicio de la página
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
-    <div>Footer</div>
-  )
-}
+    <FooterWrapper>
+      <FooterContainer>
+        <FooterUpperContainer>
+          <LogoContainer onClick={() => navigate("/")}>
+            <FooterLogoImgContainer>
+              <FooterLogoImg src={HubReserveLogo} />
+            </FooterLogoImgContainer>
+          </LogoContainer>
 
-export default Footer
+          <FooterLinkWrapper>
+            <FooterLinkContainer>
+              {variant === "" ? (
+                <>
+                  {/* <FooterLink onClick={() => navigate(`${ROUTES.RESERVE}`)}>Reservar </FooterLink>
+                  <FooterLink onClick={() => navigate(`${ROUTES.FAVORITES}`)}>Favoritos</FooterLink>
+                  <FooterLink onClick={() => navigate(`${ROUTES.CALENDAR}`)}>Calendario</FooterLink>
+                  <FooterLink onClick={() => navigate(`${ROUTES.RESERVATIONS}`)}>Mis Reservaciones</FooterLink>
+                  <FooterLink onClick={() => navigate(`${ROUTES.PROFILE}`)}>Perfil</FooterLink> */}
+                </>
+              ) : variant === "admin" ? (
+                <>
+                  {/* <FooterLink onClick={() => navigate(`${ROUTES.ADMIN}${ROUTES.PANEL}`)}>Panel </FooterLink>
+                  <FooterLink onClick={() => navigate(`${ROUTES.ADMIN}${ROUTES.ADMIN_RESERVATIONS}`)}>Reservaciones</FooterLink>
+                  <FooterLink onClick={() => navigate(`${ROUTES.ADMIN}${ROUTES.STATISTICS}`)}>Estadísticas</FooterLink>
+                  <FooterLink onClick={() => navigate(`${ROUTES.ADMIN}${ROUTES.USERS}`)}>Users</FooterLink>
+                  <FooterLink onClick={() => navigate(`${ROUTES.ADMIN}${ROUTES.PROFILE}`)}>Perfil</FooterLink> */}
+                </>
+              ) : null}
+            </FooterLinkContainer>
+          </FooterLinkWrapper>
+        </FooterUpperContainer>
+
+        <FooterBelowContainer>
+          <FooterDivider />
+          {isMobile ? (
+            <>
+              <Grid
+                container
+                alignItems="center"
+                justifyContent="center"
+                spacing={1}
+              >
+                <Grid item>
+                  <IconButton
+                    sx={{
+                      "&:hover": {
+                        color: "#fff",
+                        transition: "color 0.4s ease-in-out",
+                        opacity: 0.7,
+                        svg: {
+                          color: "#fff",
+                          transition: "color 0.4s ease-in-out",
+                        },
+                      },
+                    }}
+                    style={{ marginTop: "10px" }}
+                  >
+                    <Facebook sx={{ fontSize: 36, color: "#a0a2aa" }} />
+                  </IconButton>
+                </Grid>
+                <Grid item>
+                  <IconButton
+                    sx={{
+                      "&:hover": {
+                        color: "#fff",
+                        transition: "color 0.4s ease-in-out",
+                        opacity: 0.7,
+                        svg: {
+                          color: "#fff",
+                          transition: "color 0.4s ease-in-out",
+                        },
+                      },
+                    }}
+                    style={{ marginTop: "10px" }}
+                  >
+                    <LinkedIn sx={{ fontSize: 36, color: "#a0a2aa" }} />
+                  </IconButton>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <FooterCopyrightContainer>
+                  <FooterCopyrightIcon>
+                    <CopyrightIcon sx={{ fontSize: 15 }} />
+                  </FooterCopyrightIcon>
+                  <FooterCopyrightText>
+                    &nbsp; 2023 Hub Reserve All rights reserved.
+                  </FooterCopyrightText>
+                </FooterCopyrightContainer>
+              </Grid>
+            </>
+          ) : (
+            <>
+              <Grid
+                container
+                alignItems="center"
+                justifyContent="space-between"
+                margin-top="1rem"
+                spacing={1}
+              >
+                <Grid item>
+                  <FooterCopyrightContainer>
+                    <FooterCopyrightIcon>
+                      <CopyrightIcon sx={{ fontSize: 15 }} />
+                    </FooterCopyrightIcon>
+                    <FooterCopyrightText>
+                      &nbsp;© 2023 Hub Reserve All rights reserved.
+                    </FooterCopyrightText>
+                  </FooterCopyrightContainer>
+                </Grid>
+
+                <Grid item>
+                  <IconButton
+                    sx={{
+                      "&:hover": {
+                        color: "#fff",
+                        transition: "color 0.4s ease-in-out",
+                        opacity: 0.7,
+                        svg: {
+                          color: "#fff",
+                          transition: "color 0.4s ease-in-out",
+                        },
+                      },
+                    }}
+                  >
+                    <Facebook sx={{ fontSize: 30, color: "#a0a2aa" }} />
+                  </IconButton>
+                  <IconButton
+                    sx={{
+                      "&:hover": {
+                        color: "#fff",
+                        transition: "color 0.4s ease-in-out",
+                        opacity: 0.7,
+                        svg: {
+                          color: "#fff",
+                          transition: "color 0.4s ease-in-out",
+                        },
+                      },
+                    }}
+                  >
+                    <LinkedIn sx={{ fontSize: 30, color: "#a0a2aa" }} />
+                  </IconButton>
+                </Grid>
+              </Grid>
+            </>
+          )}
+        </FooterBelowContainer>
+      </FooterContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,1 +1,110 @@
-export {}
+import styled from "styled-components";
+import theme from "../../theme/theme";
+
+export const FooterWrapper = styled.div`
+  background-color: ${theme.color.shadow};
+  width: 100%;
+  padding: 2em 0;
+  margin-bottom: 0px;
+  font-family: "Inter", sans-serif;
+`;
+
+export const FooterContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+export const FooterUpperContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 4em;
+`;
+
+export const FooterLogoImgContainer = styled.div`
+  margin: 1em 0 1em 1em;
+`;
+
+export const FooterLogoImg = styled.img`
+  height: 2.7em;
+  width: auto;
+`;
+
+export const FooterLinkWrapper = styled.div``;
+
+export const FooterLinkContainer = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const FooterLink = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+  color: #f5f5f5;
+  margin: 1em;
+  font-size: 1em;
+
+  &:hover {
+    color: ${theme.color.honey};
+    transition: color 0.4s ease;
+  }
+`;
+
+export const FooterBelowContainer = styled.div``;
+
+export const FooterDivider = styled.hr`
+  border: none;
+  height: 1px;
+  background-color: #3a3b43;
+`;
+
+export const FooterCopyrightContainer = styled.div`
+  color: #a0a2aa;
+  text-align: left;
+  display: flex;
+  align-items: flex-start;
+  @media (max-width: 1024px) {
+    margin-top: 1rem;
+  }
+`;
+
+export const FooterCopyrightIcon = styled.div``;
+
+export const FooterCopyrightText = styled.div`
+  color: #a0a2aa;
+  text-align: left;
+  font-size: 0.8em;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  gap: -0.5em;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const TextContainer = styled.div`
+  display: inline-block;
+  margin: 1em 1em 1em 0;
+`;
+
+export const UPText = styled.span`
+  color: ${theme.color.honey};
+  font-size: 1.5em;
+  font-weight: 700;
+`;
+
+export const ElectricText = styled.span`
+  color: ${theme.color.white};
+  font-size: 1.5em;
+  font-weight: 700;
+`;
