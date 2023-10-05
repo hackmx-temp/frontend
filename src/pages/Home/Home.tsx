b1c4ae0f7dedc094d5987f3fbb96bf9cb1a7cf82
Ernesto Miranda <ernomitrovic@zohomail.com>
2023-10-04 23:26:23 -0600

    Sections fixed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,23 @@
 import React from "react";
 import { AboutUs } from "../../components/AboutUs";
 import { Carousel } from "../../components/Carousel";
-import Button from '@mui/material/Button';
-import Header from "../../componentes/Header";
-import { Link } from 'react-router-dom';
-
-
 import Chronogram from "../../components/Chronogram/Chronogram";
 import { MapComponent } from "../../components/MapComponent";
-import { Container, Grid } from "@mui/material";
+import { Button, Container, Grid } from "@mui/material";
 import { MainContainer } from "./styles";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <div style={{ position: 'relative', textAlign: 'center' }}>
+      <div style={{ position: 'relative', textAlign: 'center' }}>
           <img src="/HACK2023INICIO.png" alt="Tu imagen" className="imagen-home" style={{ width: '100%' }} />
-          <Link to="/registro"> {/* Agrega el enlace aquí */}
+          <Link to="/registro">
             <Button variant="contained" style={{ position: 'absolute', top: '80%', left: '50%', transform: 'translate(-50%, -50%)', width: '18%', height: '9%', textTransform: 'uppercase', borderRadius: '26px', color: '#FFFFFF', fontSize: '2vw' }}>
               Registrate
             </Button>
           </Link>
         </div>
-      </div>
       <div>
         <AboutUs />
       </div>
@@ -50,9 +44,37 @@ const Home = () => {
           imageWidth="150px"
         />
       </div>
-      <div>
-        <p>Section 5</p>
-      </div>
+      <MainContainer>
+        <Container
+          style={{
+            // height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            backgroundColor: "#e4caa5",
+          }}
+        >
+          <Grid
+            container
+            spacing={3}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              style={{
+                paddingLeft: "5rem",
+              }}
+            >
+              <Chronogram />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <MapComponent />
+            </Grid>
+          </Grid>
+        </Container>
+      </MainContainer>
     </div>
   );
 };
