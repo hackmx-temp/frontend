552e2330efe9297b7331e36ea27324dae4ffd6f3
julietteceb16 <126595208+discovery23@users.noreply.github.com>
2023-10-04 20:05:54 -0600

    header y sección 1

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { Sponsors } from "../../components/Sponsors";
+import Button from '@mui/material/Button';
+import Header from "../../componentes/Header";
+import { Link } from 'react-router-dom';
+
+
 
 const Home = () => {
   return (
     <div>
+      <Header></Header>
       <div>
-        <p>Section 1</p>
-      </div>
+      <div style={{ position: 'relative', textAlign: 'center' }}>
+  <img src="/HACK2023INICIO.png" alt="Tu imagen" className="imagen-home" style={{ width: '100%' }} />
+  <Link to="/registro"> {/* Agrega el enlace aquí */}
+    <Button variant="contained" style={{ position: 'absolute', top: '80%', left: '50%', transform: 'translate(-50%, -50%)', width: '18%', height: '9%', textTransform: 'uppercase', borderRadius: '26px', color: '#FFFFFF', fontSize: '2vw' }}>
+      Registrate
+    </Button>
+  </Link>
+</div>
+    </div>
+
+
+
       <div>
         <p>Section 2</p>
       </div>
