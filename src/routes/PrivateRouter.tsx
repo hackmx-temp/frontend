7b20350f6efdf8aa5c69f02a32fe6502bfb2f426
Ernesto Miranda <ernomitrovic@zohomail.com>
2023-10-23 08:45:50 -0600

    Optimizando estructura

--- src/routes/PrivateRouter.tsx
+++ src/routes/PrivateRouter.tsx
@@ -1,11 +1,9 @@
-import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import Header from "../components/Header";
 
 const PrivateRouter = () => {
   const isAuthenticated = !!localStorage.getItem("token"); // Check if the token is present
-  const navigate = (to: string) => <Navigate to={to} replace={true} />;
   return (
     <>
       {isAuthenticated ? (
@@ -14,7 +12,7 @@ const PrivateRouter = () => {
           <Header /> <Outlet /> <Footer />{" "}
         </>
       ) : (
-        navigate("/sign-in")
+        <Navigate to="/sign-in" replace={true} />
       )}
     </>
   );
